29c8c9df0e0ef667043dd847f9e0d2e37e45be7a
achampagne1 <53927114+achampagne1@users.noreply.github.com>
2024-09-09 17:58:05 -0500

    Add files via upload

---
+++ Mouse_Demo_PIC32MX.X/build/default/production/usb_descriptors.o.d
@@ -0,0 +1,21 @@
+build/default/production/usb_descriptors.o: usb_descriptors.c usb.h \
+ Compiler.h usb_config.h usb_common.h usb_ch9.h usb_device.h usb_hal.h \
+ usb_hal_pic32.h usb_function_hid.h
+
+usb.h:
+
+Compiler.h:
+
+usb_config.h:
+
+usb_common.h:
+
+usb_ch9.h:
+
+usb_device.h:
+
+usb_hal.h:
+
+usb_hal_pic32.h:
+
+usb_function_hid.h:
